1e4d8b04a2b857f4b3bb1dcb986bd033d065f634
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-06-15 22:37:23 +0330

    feat: create section1 component and finish it

---
+++ src/components/section1.home.tsx
@@ -0,0 +1,19 @@
+import logo from "../assets/images/logo-quotable.png";
+
+const Section1Home = () => {
+  return (
+    <>
+      <section className="h-screen w-screen bg-section-1-bg bg-cover flex items-center justify-center">
+        <div className="w-120 h-100">
+          <img width="600" src={logo} alt="Logo" />
+        </div>
+        <p className="w-80 font-Poppins text-hblue text-xl">
+          Serach in the words of elders and take advantage of their knowledge
+          and experience.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Section1Home;
