9c7ee811368d3fd6abeece3475bc2d7ccafa3b9c
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-06-15 22:35:01 +0330

    chore: add necessary custom styles to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "section-1-bg": "url('src/assets/images/background-image.jpg')",
+      },
+      width: {
+        "120": "37.5rem",
+      },
+      height: {
+        "100": "23.5rem",
+      },
+      fontFamily: {
+        "Agbalumo": "Agbalumo",
+        "Poppins": "Poppins",
+      },
+      colors: {
+        "hblue": "#63b0ee",
+      }
     },
   },
   plugins: [],
