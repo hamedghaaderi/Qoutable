6f54992a8cfb20ea55a064a4502676caea588911
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-06-16 21:54:38 +0330

    feat: create config of query client for all requests

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60000,
+      gcTime: 45000,
+      refetchInterval: 5 * 60000,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
