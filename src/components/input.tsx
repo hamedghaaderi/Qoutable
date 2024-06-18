18404514331264f7c6cd8581f05a36b03e86216b
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-06-18 21:09:53 +0330

    fix: replace link component instead of button element

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import SearchIcon from "./searchicon";
 
 const Input = () => {
@@ -9,9 +10,14 @@ const Input = () => {
           placeholder="Search Authors"
           className="w-9/10 rounded-3xl p-3 pl-6 outline-none rounded-r-none "
         />
-        <button className="w-1/10 rounded-3xl rounded-l-none bg-yellow p-3">
+        {/* <button className="w-1/10 rounded-3xl rounded-l-none bg-yellow p-3">
           <SearchIcon />
-        </button>
+        </button> */}
+        <Link to={"/authors"}>
+          <div className="rounded-3xl rounded-l-none bg-yellow p-3 w-full h-full">
+            <SearchIcon />
+          </div>
+        </Link>
       </div>
     </>
   );
