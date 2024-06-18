1d4b2079930718347e917978439c104e27f3cbfa
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-06-18 21:07:33 +0330

    feat: create Authors page route and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import AuthorsPage from "./pages/Authorspage";
 import Homepage from "./pages/Homepage";
 import { useRoutes } from "react-router-dom";
 const App = () => {
-  const routes = useRoutes([{ path: "/", element: <Homepage /> }]);
+  const routes = useRoutes([
+    { path: "/", element: <Homepage /> },
+    { path: "/authors", element: <AuthorsPage /> },
+  ]);
   return <>{routes}</>;
 };
 
