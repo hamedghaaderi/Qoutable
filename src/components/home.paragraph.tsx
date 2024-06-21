710caa0304a1c0daa19fc98a013cb004102e1a22
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-06-21 23:15:09 +0330

    fix: change style in section 4

--- src/components/home.paragraph.tsx
+++ src/components/home.paragraph.tsx
@@ -4,7 +4,7 @@ const HomeParagraph = ({link , p , text}: {link:string , p:string , text: string
   return (
     <>
     <div className="flex flex-col justify-between h-24 items-center">
-            <p className="font-Poppins text-md text-blue">
+            <p className="font-Poppins text-md text-hblue">
               {p}
             </p>
             <Button href={link}>{text}</Button>
