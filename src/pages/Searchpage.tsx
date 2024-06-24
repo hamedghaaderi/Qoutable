fe2f16c2376b849c4d79914cf6de91591208fe8a
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-06-24 12:31:08 +0330

    feat: add header and footer components

--- src/pages/Searchpage.tsx
+++ src/pages/Searchpage.tsx
@@ -1,9 +1,14 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+import ButtonInput from "../components/button.input";
+
 const SearchPage = () => {
   return (
     <>
-    
+      <Header btn={<ButtonInput />} />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
