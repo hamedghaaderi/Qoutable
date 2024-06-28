b365b756451a70633dfa2b4609f25b996421efd1
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-06-28 19:19:16 +0330

    refactor: create a hook for document count request

---
+++ src/hook/document.count.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import config from "../api/api.config";
+
+const useDocumentCount = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["document-count"],
+    queryFn: () => {
+      return config.get("/info/count").then((res) => res);
+    },
+    gcTime: Infinity,
+    refetchInterval: false,
+  });
+  return { data, isLoading };
+};
+
+export default useDocumentCount;
