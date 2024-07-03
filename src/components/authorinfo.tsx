4478563693f674ea889b177a0c4b00050f2539a5
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-07-03 20:50:15 +0330

    fix: fix a little bug in AuthorInfo component styles

--- src/components/authorinfo.tsx
+++ src/components/authorinfo.tsx
@@ -12,24 +12,30 @@ interface IAuthorInfo {
 const AuthorInfo = ({ name, desc, bio, link, totalQoutes }: IAuthorInfo) => {
   return (
     <>
-      <section className="bg-white rounded-3xl p-10 h-100 flex flex-col justify-between items-start">
-        <h2 className="text-2xl text-blue font-Poppins">{name}</h2>
-        <span className="text-xl text-hblue font-Poppins">{desc}</span>
-        <p className="text-lg text-justify font-Poppins text-black">{bio}</p>
-        <span className="text-blue text-lg">
-          Number Of Qoutes:{" "}
-          <span className="bg-hblue ml-2 p-2 rounded-lg text-black">
-            {totalQoutes}
-          </span>
+      <section className="bg-white rounded-3xl p-10">
+        <h2 className="text-2xl text-blue font-Poppins mb-1">{name}</h2>
+        <span className="text-xl text-hblue font-Poppins mb-3 inline-block">
+          {desc}
         </span>
-        <div className="w-64 self-end flex flex-row items-center justify-between">
-          <Button href={"/authors"}>List Authors</Button>
-          <a
-            className="pl-6 py-2 border-l-2 border-l-hblue flex w-28 justify-between text-lg items-center"
-            href={link}
-          >
-            Explore <ArrowIcon />
-          </a>
+        <p className="text-lg text-justify font-Poppins text-black mb-3">
+          {bio}
+        </p>
+        <div className="flex flex-col justify-between">
+          <span className="text-blue text-lg inline-block">
+            Number Of Qoutes:{" "}
+            <span className="bg-hblue ml-2 p-2 rounded-lg text-black">
+              {totalQoutes}
+            </span>
+          </span>
+          <div className="w-64 self-end flex flex-row items-center justify-between">
+            <Button href={"/authors"}>List Authors</Button>
+            <a
+              className="pl-6 py-2 border-l-2 border-l-hblue flex w-28 justify-between text-lg items-center"
+              href={link}
+            >
+              Explore <ArrowIcon />
+            </a>
+          </div>
         </div>
       </section>
     </>
