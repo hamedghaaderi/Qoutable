489fd7ec3b08131239d01a0f998146963e04ff9d
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-07-04 00:00:46 +0330

    feat: create authors page components and add asc/desc filter

--- src/pages/Authorspage.tsx
+++ src/pages/Authorspage.tsx
@@ -1,6 +1,55 @@
+import { useSearchParams } from "react-router-dom";
+import ArrowDownIcon from "../components/arrowdownicon";
+import ArrowUpIcon from "../components/arrowupicon";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import IsLoading from "../components/isloading";
+import LinkTo from "../components/link.to";
+import ResultItem from "../components/result.item";
+import useListAuthors from "../hook/list.authors";
+
 const AuthorsPage = () => {
+  const { isLoading, data } = useListAuthors();
+  const [, setSearchParams] = useSearchParams();
+  const handelClick = (param: string) => {
+    const query = `order=${param}`;
+    setSearchParams(query);
+  };
+
   return (
     <>
+      <Header btn={<LinkTo />} link={"/random"} innerText={"Random Qoute"} />
+      <main className="bg-back px-20 py-12">
+        <div className="mb-9">
+          <button
+            onClick={() => handelClick("desc")}
+            className="bg-white p-2 rounded-lg mr-4"
+          >
+            <ArrowUpIcon />
+          </button>
+          <button
+            onClick={() => handelClick("asc")}
+            className="bg-white p-2 rounded-lg"
+          >
+            <ArrowDownIcon />
+          </button>
+        </div>
+        {isLoading ? (
+          <IsLoading />
+        ) : (
+          data?.data.results.map((item: any) => {
+            return (
+              <ResultItem
+                key={item._id}
+                name={item.name}
+                desc={item.description}
+                slug={item.slug}
+              />
+            );
+          })
+        )}
+      </main>
+      <Footer />
     </>
   );
 };
