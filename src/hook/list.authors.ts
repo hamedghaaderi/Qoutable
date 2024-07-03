b8f7e8f91721f789ff95e72638f8ba3f7a5cef16
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-07-03 23:58:49 +0330

    feat: write a hook for send request for list authors

---
+++ src/hook/list.authors.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import config from "../api/api.config";
+import { useSearchParams } from "react-router-dom";
+
+const useListAuthors = () => {
+  const [searchParams] = useSearchParams();
+  const pageParam: string = searchParams.get("page") ?? "1";
+  const orderParam: string = searchParams.get("order") ?? "asc";
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["authors", pageParam, orderParam],
+    queryFn: () => {
+      return config
+        .get(`/authors?page=${pageParam}&order=${orderParam}`)
+        .then((res) => res);
+    },
+  });
+  return { data, isLoading };
+};
+
+export default useListAuthors;
